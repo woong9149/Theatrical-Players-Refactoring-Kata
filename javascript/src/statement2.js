599b659390d627da6ce8b0ffcb4095936287d450
woong9149 <sewoong0409@naver.com>
2023-04-12 05:21:55 +0900

    [REFACTOR] 공연료 계산기 - 본문 전체를 별도 함수로 추출

--- javascript/src/statement2.js
+++ javascript/src/statement2.js
@@ -1,6 +1,10 @@
 
 
 function statement (invoice, plays) {
+    return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
 
     for (let perf of invoice.performances) {
@@ -72,5 +76,4 @@ function statement (invoice, plays) {
         return result;
     }
 }
-
 module.exports = statement;
