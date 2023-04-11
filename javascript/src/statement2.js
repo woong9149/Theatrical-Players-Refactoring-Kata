97e5ab6f6181180ab21b0531721234dcd3231721
woong9149 <sewoong0409@naver.com>
2023-04-12 05:13:48 +0900

    [REFACTOR] 공연료 계산기 - totalAmount 함수 분리

--- javascript/src/statement2.js
+++ javascript/src/statement2.js
@@ -1,17 +1,25 @@
 
 
 function statement (invoice, plays) {
-    let totalAmount = 0;
     let result = `Statement for ${invoice.customer}\n`;
 
     for (let perf of invoice.performances) {
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-        totalAmount += amountFor(perf);
     }
 
+    let totalAmount = appleSauce();
+    
     result += `Amount owed is ${usd(totalAmount)}\n`;
     result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
+
+    function appleSauce() {
+        let totalAmount = 0;
+        for (let perf of invoice.performances) {
+            totalAmount += amountFor(perf);
+        }
+        return totalAmount;
+    }
 
     function totalVolumeCredits() {
         let volumeCredits = 0;
