dfcb30e14b715efb485ab04f66b817a282070411
woong9149 <sewoong0409@naver.com>
2023-04-12 04:44:55 +0900

    [REFACTOR] 공연료 계산기 - volumeCredits 함수로 분리

--- javascript/src/statement2.js
+++ javascript/src/statement2.js
@@ -9,17 +9,20 @@ function statement (invoice, plays) {
           minimumFractionDigits: 2 }).format;
 
     for (let perf of invoice.performances) {
-        // add volume credits
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
-        // print line for this order
+        volumeCredits += volumeCreditsFor(perf)
         result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
     result += `Amount owed is ${format(totalAmount/100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
+
+    function volumeCreditsFor(perf) {
+        let volumeCredits = 0;
+        volumeCredits += Math.max(perf.audience - 30, 0);
+        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+        return volumeCredits;
+    }
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
