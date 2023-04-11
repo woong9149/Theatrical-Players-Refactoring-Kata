75e4abeacd052881c956903b18b1c3e8ee9d8b6e
woong9149 <sewoong0409@naver.com>
2023-04-12 05:35:59 +0900

    [REFACTOR] 공연료 계산기 - playFor() renderPlainText()로 옮기기 - playFor() 호출부 수정

--- javascript/src/statement2.js
+++ javascript/src/statement2.js
@@ -8,7 +8,12 @@ function statement (invoice, plays) {
 
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance);
+        result.play = playFor(result);
         return result;
+    }
+
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
     }
 }
 
@@ -16,7 +21,7 @@ function renderPlainText(data, plays) {
     let result = `Statement for ${data.customer}\n`;
 
     for (let perf of data.performances) {
-        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+        result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     }
 
     result += `Amount owed is ${usd(totalAmount())}\n`;
@@ -51,20 +56,16 @@ function renderPlainText(data, plays) {
         let volumeCredits = 0;
         volumeCredits += Math.max(aPerformance.audience - 30, 0);
 
-        if ("comedy" === playFor(aPerformance).type)
+        if ("comedy" === aPerformance.play.type)
             volumeCredits += Math.floor(aPerformance.audience / 5);
 
         return volumeCredits;
     }
 
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID];
-    }
-
     function amountFor(aPerformance) {
         let result = 0;
 
-        switch (playFor(aPerformance).type) {
+        switch (aPerformance.play.type) {
             case "tragedy":
                 result = 40000;
                 if (aPerformance.audience > 30) {
@@ -79,7 +80,7 @@ function renderPlainText(data, plays) {
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+                throw new Error(`unknown type: ${aPerformance.play.type}`);
         }
         return result;
     }
